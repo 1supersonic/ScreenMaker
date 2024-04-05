5a6c7bcd329093f205c76753e869130e3edae8d1
Alexander Kuznetsov <1kuznetsovup@gmail.com>
2024-04-05 18:04:13 +0300

    Update script_position.js

--- scripts/script_position.js
+++ scripts/script_position.js
@@ -101,7 +101,7 @@ function save_position() {
     unr_pnl_percent = ((unr_pnl / margin) * 100).toFixed(2); // верно
     unr_pnl_rounded = parseFloat(unr_pnl).toFixed(2);
     r_pnl = parseFloat(margin * 0.01).toFixed(4);
-    r_pnl_rounded = r_pnl.toFixed(2);
+    r_pnl_rounded = parseFloat(r_pnl).toFixed(2);
     
 
     // Отрисовка элементов в зависимости от Long / Short
