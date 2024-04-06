ec12af1e895e7e30590dc0ccfa9d0c62d5033729
Alexander Kuznetsov <1kuznetsovup@gmail.com>
2024-04-06 20:13:15 +0300

    Update script_position.js

--- scripts/script_position.js
+++ scripts/script_position.js
@@ -119,7 +119,10 @@ function save_position() {
     
     
     // цвет PnL в зависимости от наличия минуса 
-    if (String(unr_pnl)[0] == "-") {
+    if (String(unr_pnl)[0] == "-" and longshort == "Long") {
+        document.getElementById("unr_pnl").style.color = text_color_red;
+        document.getElementById("unr_pnl_rounded").style.color = text_color_red;
+    } else if (String(unr_pnl)[0] != "-" and longshort == "Short") {
         document.getElementById("unr_pnl").style.color = text_color_red;
         document.getElementById("unr_pnl_rounded").style.color = text_color_red;
     } else {
