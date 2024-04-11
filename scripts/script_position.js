4680daf41451088e0b4ceb7a2aec798817e367fe
Alexander Kuznetsov <1kuznetsovup@gmail.com>
2024-04-11 12:17:20 +0300

    .

--- scripts/script_position.js
+++ scripts/script_position.js
@@ -55,7 +55,11 @@ setInterval(getCoinPrice, 2000); // каждые 2,5 секунды
 
 // Автоматическое заполнение графы текущего времени
 function setCurrentTime() {
-    alert("функция пока недоступна");
+    let time = new Date();
+    let hours = String(time.getHours());
+    let minutes = String(time.getMinutes());
+    let current_time = hours + ":" + minutes;
+    document.getElementById("input_time").setAttribute("value", current_time);
 }
 
 
