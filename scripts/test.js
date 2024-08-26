ca8f947dfa7cef2649b61b684b5a62d7880c9228
Alexander <1kuznetsovup@gmail.com>
2024-08-26 20:52:42 +0300

    .

---
+++ scripts/test.js
@@ -0,0 +1,7 @@
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById('data');
+        container.innerHTML = JSON.stringify(data, null, 2);
+        console.log(data);
+    });
